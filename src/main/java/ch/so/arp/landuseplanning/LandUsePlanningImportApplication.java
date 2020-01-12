a5d129eedf0f627c764d9f6696c36e76c442dc37
Stefan Ziegler <edi.gonzales@gmail.com>
2020-01-12 18:53:12 +0100

    initial commit

---
+++ src/main/java/ch/so/arp/landuseplanning/LandUsePlanningImportApplication.java
@@ -0,0 +1,13 @@
+package ch.so.arp.landuseplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandUsePlanningImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandUsePlanningImportApplication.class, args);
+	}
+
+}
